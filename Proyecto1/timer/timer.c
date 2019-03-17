09b051112148781f7bf9e0d4812b73233d092d4c
jafet95 <jafet.a15@gmail.com>
2019-03-17 10:49:03 -0600

    Add timer/ directory containing timer handling functions

---
+++ Proyecto1/timer/timer.c
@@ -0,0 +1,34 @@
+// System includes
+#include <stdio.h>
+#include <time.h>
+#include <signal.h>
+#include <sys/time.h>
+
+// Custom includes
+#include "timer.h"
+
+// Properties.
+
+struct itimerval timer;
+int timeUnit = 1; // Must be 1000 to be milliseconds, 1 only for testing purposes.
+void (*callback)(int);
+
+/* 
+    Configures the timer.
+*/
+void setUpTimer (void (*pCallback)(int)) {
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 0;
+    timer.it_value.tv_sec = 0;
+
+    callback = pCallback;
+}
+
+/*
+    Sets the timer and fires it.
+*/
+void setTimer (int pTime) {
+    signal(SIGALRM, callback);    
+    timer.it_value.tv_usec = pTime * timeUnit;
+    setitimer(ITIMER_REAL, &timer, 0);
+}
