1403c0bcbc6bdf12c9e4758e49663b41c2f9057f
Jose Viquez <jose.viquezh@gmail.com>
2019-05-25 09:49:07 -0600

    Added LLF scheduling algorithm
    The algorithm for LLF was added as seen in class: Laxity = d - a - c
    where d is the deadline, a is the time and c is the reamining
    computation time

--- Proyecto3/scheduler/scheduler.c
+++ Proyecto3/scheduler/scheduler.c
@@ -47,18 +47,26 @@ void RMSortReadyQueue (QueueItem * p_ready_queue, int p_size) {
 }
 
 void EDFSortReadyQueue (QueueItem * p_ready_queue, int p_size) {
-    
+
     for (int i = 0; i < p_size; i++) {
         p_ready_queue[i].priority -= 1;
     }
-    
+
     RMSortReadyQueue (p_ready_queue, p_size);
 }
 
 void LLFSortReadyQueue (QueueItem * p_ready_queue, int p_size) {
+    int a = elapsed_time;
     for (int i = 0; i < p_size; i++) {
-        //laxitiy = x = d - a - c
-        //p_ready_queue[i].priority = ;
+        int x = 0, d = 0, c = 0, period = 0;
+        d = period = p_ready_queue[i].task.period;
+        while(d < a){
+            d += period;
+        }
+        c = p_ready_queue[i].task.computation_time;
+        x = d - a - c;
+
+        p_ready_queue[i].priority = x;
     }
 
     RMSortReadyQueue (p_ready_queue, p_size);
@@ -84,7 +92,7 @@ void SortReadyQueue () {
 void InitScheduler (int p_tasks_length, Task * p_tasks, int p_algorithm) {
     // Queue initialization.
     ready_queue = calloc(2 * p_tasks_length, sizeof(QueueItem)); //; in case deadline of a task is missed
-    
+
     for (int i = 0; i < p_tasks_length; i++) {
         ready_queue[i] = (QueueItem) { p_tasks[i], p_tasks[i].period, p_tasks[i].computation_time, 0, 0 };
     }
@@ -101,12 +109,12 @@ void InitScheduler (int p_tasks_length, Task * p_tasks, int p_algorithm) {
     end_flag = 0;
     deadline_missed_flag = 0;
 
-    SortReadyQueue(p_algorithm);
+    SortReadyQueue();
 }
 
 void RemoveHeadFromQueue () {
     for (int i = 1; i < current_items_in_queue; i++) {
-        ready_queue[i - 1] = ready_queue[i]; 
+        ready_queue[i - 1] = ready_queue[i];
     }
     current_items_in_queue -= 1;
 }
@@ -120,7 +128,7 @@ void CheckForDeadlines () {
         }
     }
 
-    //if (deadline_missed_flag > 0) deadline_missed_flag = deadline_missed_flag / 2; 
+    //if (deadline_missed_flag > 0) deadline_missed_flag = deadline_missed_flag / 2;
 }
 
 void AddTasksToQueue () {
@@ -134,7 +142,7 @@ void AddTasksToQueue () {
     }
 
     if (total_added == total_tasks) end_flag = 1;
-    
+
     CheckForDeadlines ();
 }
 
@@ -148,7 +156,7 @@ void ApplyAdvancedToQueue () {
 
     history_size += 1;
 
-    if (current_items_in_queue > 0){ 
+    if (current_items_in_queue > 0){
         ready_queue[0].remaining_periods -= 1;
         if (ready_queue[0].remaining_periods == 0) RemoveHeadFromQueue();
     }
@@ -175,13 +183,13 @@ int GetHistorySize () {
 
 int RunScheduling () {
     while (end_flag == 0 && deadline_missed_flag == 0) {
-        
+
         elapsed_time += 1;
         ApplyAdvancedToQueue();
         SortReadyQueue();
-        
+
         /*printf("\n============= Time elapsed %i ==============\n", elapsed_time);
-        
+
         if (!history[history_size - 1].null) printf("Running task: %i in elapsed_time = %i\n", history[history_size - 1].task.id, elapsed_time);
         else printf("Free period in elapsed_time = %i\n", elapsed_time);
 
@@ -194,4 +202,6 @@ int RunScheduling () {
     }
 
     printf("\nend_flag = %i, deadline_flag = %i\n\n", end_flag, deadline_missed_flag);
+
+    return 0;
 }
